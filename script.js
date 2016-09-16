9e5aa4aad29a8e9775403f7cf56ff7c32b839567
System Administrator <root@Tilt-Designer.local>
2016-09-16 18:22:23 +0100

    real numbers

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ var characters = [
     {
         name: 'Batman',
         nicename: 'Batman',
-        powers: [5, 25, 28, 35],
+        powers: [76, 8, 82, 19],
         blackcardstart: 'Quick! To the',
         blackcardend: '-mobile',
         whitecard: 'KAPOW!'
@@ -25,7 +25,7 @@ var characters = [
     {
         name: 'Joker',
         nicename: 'The Joker',
-        powers: [10, 20, 30, 40],
+        powers: [74, 15, 86, 79],
         blackcardstart: '',
         blackcardend: 'so serious?',
         whitecard: 'smile'
@@ -33,7 +33,7 @@ var characters = [
     {
         name: 'Trump',
         nicename: 'Donald Trump',
-        powers: [10, 20, 30, 40],
+        powers: [100, 0, 99, 1],
         blackcardstart: 'I will build a great',
         blackcardend: 'and make Mexico pay. Mark my words.',
         whitecard: 'Trump steaks'
@@ -41,7 +41,7 @@ var characters = [
     {
         name: 'nigel-farage',
         nicename: 'Nigel Farage',
-        powers: [10, 20, 30, 40],
+        powers: [99, 82, 87, 0],
         blackcardstart: 'The Euro Titanic has now hit the iceberg - and there simply aren’t enough',
         blackcardend: 'to go around',
         whitecard: 'kick \'em out'
@@ -49,7 +49,7 @@ var characters = [
     {
         name: 'The-Starks',
         nicename: 'The Stark Family',
-        powers: [10, 20, 30, 40],
+        powers: [6, 66, 72, 59],
         blackcardstart: '',
         blackcardend: 'is coming',
         whitecard: 'King of the North'
@@ -57,9 +57,9 @@ var characters = [
     },
     {
         name: 'The-lanisters',
-        nicename: 'The lanister Family',
-        powers: [10, 20, 30, 40],
-        blackcardstart: 'A lanister always pays his',
+        nicename: 'The Lanister Family',
+        powers: [88, 91, 82, 0],
+        blackcardstart: 'A Lanister always pays his',
         blackcardend: '',
         whitecard: 'the Kingslayer'
 
@@ -67,7 +67,7 @@ var characters = [
     {
         name: 'J-Bieber',
         nicename: 'Bieber',
-        powers: [10, 20, 30, 40],
+        powers: [18, 56, 1, 98],
         blackcardstart: 'Baby, baby,',
         blackcardend: 'oh!',
         whitecard: 'I\'m sorry'
@@ -77,7 +77,7 @@ var characters = [
     {
         name: 'Lemon-Grab',
         nicename: 'Lemongrab',
-        powers: [10, 20, 30, 40],
+        powers: [100, 98, 87, 0],
         blackcardstart: '',
         blackcardend: 'IS UNACCEPTABLE!',
         whitecard: 'my glob'
@@ -85,7 +85,7 @@ var characters = [
     {
         name: 'Finn',
         nicename: 'Finn',
-        powers: [10, 20, 30, 40],
+        powers: [100, 99, 83, 1],
         blackcardstart: 'What time is it?',
         blackcardend: 'time!',
         whitecard: 'totally mathematical'
@@ -93,7 +93,7 @@ var characters = [
     {
         name: 'mary-berry',
         nicename: 'Mary Berry',
-        powers: [10, 20, 30, 40],
+        powers: [0, 0, 1, 200],
         blackcardstart: 'A disappointing',
         blackcardend: 'and a soggy bottom',
         whitecard: 'apple pie'
