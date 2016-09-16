d9324ea7698fcb397beb8bf02c66c5adbec223a8
System Administrator <root@Tilt-Designer.local>
2016-09-16 18:13:10 +0100

    cards and shit

--- script.js
+++ script.js
@@ -1,81 +1,101 @@
 var characters = [
     {   name: 'Jesus',
         nicename: 'Jesus Christ',
-        powers: [10, 20, 30, 40]
+        powers: [10, 20, 30, 40],
+        blackcardstart: 'Turning water into',
+        blackcardend: 'since 6BC',
+        whitecard: 'walking on water'
     },
     {
         name: 'Devil',
         nicename: 'Satan',
-        powers: [5, 25, 28, 35]
+        powers: [5, 25, 28, 35],
+        blackcardstart: '...and you’d sell your soul for',
+        blackcardend: '?',
+        whitecard: 'the devil'
     },
     {
         name: 'Batman',
         nicename: 'Batman',
-        powers: [5, 25, 28, 35]
+        powers: [5, 25, 28, 35],
+        blackcardstart: 'Quick! To the',
+        blackcardend: '-mobile',
+        whitecard: 'KAPOW!'
     },
     {
         name: 'Joker',
         nicename: 'The Joker',
-        powers: [10, 20, 30, 40]
+        powers: [10, 20, 30, 40],
+        blackcardstart: '',
+        blackcardend: 'so serious?',
+        whitecard: 'smile'
     },
     {
         name: 'Trump',
         nicename: 'Donald Trump',
-        powers: [10, 20, 30, 40]
+        powers: [10, 20, 30, 40],
+        blackcardstart: 'I will build a great',
+        blackcardend: 'and make Mexico pay. Mark my words.',
+        whitecard: 'Trump steaks'
     },
     {
-        name: 'Taylor-Swift',
-        'fighting ability': '',
-        'scariness': '',
-        'intelligence': '',
-        'social media prowess': '',
-        'bullshit-o-meter': '',
-        'fashion sense': ''
-    },
-    {
-        name: 'Kanye',
-        'fighting ability': '',
-        'scariness': '',
-        'intelligence': '',
-        'social media prowess': '',
-        'bullshit-o-meter': '',
-        'fashion sense': ''
+        name: 'nigel-farage',
+        nicename: 'Nigel Farage',
+        powers: [10, 20, 30, 40],
+        blackcardstart: 'The Euro Titanic has now hit the iceberg - and there simply aren’t enough',
+        blackcardend: 'to go around',
+        whitecard: 'kick \'em out'
     },
     {
         name: 'The-Starks',
         nicename: 'The Stark Family',
-        powers: [10, 20, 30, 40]
+        powers: [10, 20, 30, 40],
+        blackcardstart: '',
+        blackcardend: 'is coming',
+        whitecard: 'King of the North'
 
     },
     {
-        name: 'The-Lannisters',
-        nicename: 'The Lannister Family',
-        powers: [10, 20, 30, 40]
+        name: 'The-lanisters',
+        nicename: 'The lanister Family',
+        powers: [10, 20, 30, 40],
+        blackcardstart: 'A lanister always pays his',
+        blackcardend: '',
+        whitecard: 'the Kingslayer'
 
     },
     {
         name: 'J-Bieber',
         nicename: 'Bieber',
-        powers: [10, 20, 30, 40]
+        powers: [10, 20, 30, 40],
+        blackcardstart: 'Baby, baby,',
+        blackcardend: 'oh!',
+        whitecard: 'I\'m sorry'
 
     },
 
     {
         name: 'Lemon-Grab',
-        'fighting ability': '',
-        'scariness': '',
-        'intelligence': '',
-        'social media prowess': '',
-        'bullshit-o-meter': '',
-        'fashion sense': ''
+        nicename: 'Lemongrab',
+        powers: [10, 20, 30, 40],
+        blackcardstart: '',
+        blackcardend: 'IS UNACCEPTABLE!',
+        whitecard: 'my glob'
     },
     {
         name: 'Finn',
-        'fighting ability': '',
-        'scariness': '',
-        'intelligence': '',
-        'social media prowess': '',
-        'bullshit-o-meter': '',
-        'fashion sense': ''
+        nicename: 'Finn',
+        powers: [10, 20, 30, 40],
+        blackcardstart: 'What time is it?',
+        blackcardend: 'time!',
+        whitecard: 'totally mathematical'
+    },
+    {
+        name: 'mary-berry',
+        nicename: 'Mary Berry',
+        powers: [10, 20, 30, 40],
+        blackcardstart: 'A disappointing',
+        blackcardend: 'and a soggy bottom',
+        whitecard: 'apple pie'
     },
 ]
